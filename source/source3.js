8055f023790f4eb4e78d6b140773f40981e8d250
tomsj <csj504@qq.com>
2020-12-19 10:10:11 +0800

    add

--- source/source3.js
+++ source/source3.js
@@ -46,7 +46,7 @@ var source3 = {
   ruleSearchTitle: 'class.con@tag.h3@text',
   ruleSearchUpdate: '',
   ruleSearchUrl:
-  '!http://ssoonn.com/comicsearch/s.aspx?d=123&s=searchKey@Header:{"User-Agent": "Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19"}',
+  '!http://ssoonn.com/comicsearch/s.aspx?d=123&s=searchKey@Header:{"Host": "ssoonn.com","User-Agent": "Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19"}',
 
 
   sort: '3',
