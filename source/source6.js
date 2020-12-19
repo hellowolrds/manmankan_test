26180304ee230ff7198192acb3f219bd01707651
tomsj <csj504@qq.com>
2020-12-19 16:43:30 +0800

    add

--- source/source6.js
+++ source/source6.js
@@ -29,14 +29,14 @@ var source6 = {
   // 推荐
 
   ruleFindAuthor: '',
-  ruleFindCid: '$.comic_id',
+  ruleFindCid: '$.UrlKey',
   ruleFindCoverUrl:
-  '$.cover',
-  ruleFindList: '$.comic_list.*',
-  ruleFindTitle: '$.name',
-  ruleFindUpdate: '',
+  '$.ShowConver',
+  ruleFindList: '$.UpdateComicItems.*',
+  ruleFindTitle: '$.Content',
+  ruleFindUpdate: '$.LastUpdateTime',
   ruleFindUrl:
-  '',
+  '全部::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=0&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&热血::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=31&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&恋爱::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=26&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&校园::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=1&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&百合::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=3&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&耽美::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=27&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&冒险::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=2&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&后宫::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=8&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&科幻::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=25&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&战争::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=12&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&悬疑::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=17&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&推理::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=33&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&搞笑::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=37&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&奇幻::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=14&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&魔法::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=15&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&恐怖::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=29&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&神鬼::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=20&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&历史::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=4&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&同人::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=30&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&运动::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=34&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&绅士::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=36&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics&&机战::http://www.dm5.com/dm5.ashx@pagesize=68&pageindex=searchPage&tagid=40&areaid=0&status=0&usergroup=0&pay=&data[group_id]=-1&char=&sort=10&action=getclasscomics',
 
   // 搜索
 
