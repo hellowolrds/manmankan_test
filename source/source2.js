caa71758634a2b01be1320ebd20f6db5e7b24558
tomsj <csj504@qq.com>
2020-12-19 08:46:05 +0800

    add

--- source/source2.js
+++ source/source2.js
@@ -5,21 +5,28 @@ var source2 = {
   enable: true,
   imgHeaders:
   'function getHeader(){return Headers.of("Referer","http://m.dmzj.com/")}function getHeader2(url){return getHeader()}function getHeader3(){return getHeader()};',
-  ruleChapterList: 'class.chapterlist@tag.li',
-  ruleChapterName: 'text',
-  ruleChapterUrl: 'data-id',
-  ruleComicAuthor: 'class.comic-info@class.author@text',
-  ruleComicContent:
-  'importPackage(Packages.org.json);importPackage(Packages.java.lang);importClass(Packages.java.util.ArrayList);importPackage(Packages.com.reader.comic.model);function parseImages(){var list=new ArrayList();var jsonObject1=new JSONObject(content);var getData=new JSONObject(jsonObject1.get("data").toString());var jsonArray=new JSONArray(getData.getString("chapter_list"));for(var j=0;j<jsonArray.length();j++){var object=jsonArray.getJSONObject(j);var chapter_id=object.get("chapter_id").toString();if(chapter_id.equals(path)){var count=Integer.parseInt(object.getString("end_var"));for(var i=0;i<count;i++){var img="http://mhpic.zymkcdn.com/comic/";var img_type=new JSONObject(object.getString("chapter_image"));img=img+img_type.getString("middle");img=img.replace("$$",(i+1)+"");var imageUrl=new ImageUrl(i+1,img,false);list.add(imageUrl)}}}return list};',
+ 
+  // 漫画详情
+  ruleComicInfoUrl: 'http://v2.api.dmzj.com/comic/%s.json',
+  ruleComicTitle: '$.title',
+  ruleComicAuthor: '$.authors[*].tag_name',
   ruleComicCover:
-  'class.comic-info@class.comic-item@class.thumbnail@tag.img@data-src',
-  ruleComicInfoUrl: 'https://m.zymk.cn/%s',
-  ruleComicInstro: 'id.comicDetailTab@class.comic-detail@class.content@text',
+  '$.cover',
+  ruleComicInstro: '$.description',
   ruleComicStatus: false,
-  ruleComicTitle: 'class.comic-info@tag.h1@text',
-  ruleComicUpdate: 'id.updateTime@text',
+  ruleComicUpdate: '$.last_updatetime@js:java.getFormatTime("yyyy-MM-dd", result*1000)',
+  ruleChapterList: '$.chapters..data.*',
+  ruleChapterName: '$.chapter_title',
+  ruleChapterUrl: '$.chapter_id',
+
+  // 图片详情
   ruleContentUrl:
   'http://api.zymk.cn/app_api/v5/getcomicinfo/?comic_id=param1',
+  ruleComicContent:
+  '',
+
+  // 推荐
+
   ruleFindAuthor: '$.comic_feature',
   ruleFindCid: '$.comic_id',
   ruleFindCoverUrl:
@@ -28,13 +35,19 @@ var source2 = {
   ruleFindTitle: '$.comic_name',
   ruleFindUrl:
   '',
-  ruleSearchAuthor: '$.comic_author',
-  ruleSearchCid: '$.id',
+
+  // 搜索
+
+  ruleSearchAuthor: 'comic_author',
+  ruleSearchCid: 'id',
   ruleSearchCoverUrl: 'comic_cover',
   ruleSearchList: '@js:function getData(){eval(result);return g_search_data;}getData();',
   ruleSearchTitle: 'comic_name',
+  ruleSearchUpdate: '',
   ruleSearchUrl:
   '!http://s.acg.dmzj.com/comicsum/search.php?s=searchKey',
+
+
   sort: '2',
   type: 2 
 };
