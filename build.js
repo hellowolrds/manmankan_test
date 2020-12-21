971c985258288bcbe5b8d9ef71e1b4df8b5ee370
tomsj <csj504@qq.com>
2020-12-21 12:34:02 +0800

    add

--- build.js
+++ build.js
@@ -26,6 +26,7 @@ var source23 = require("./source/source23");
 var source24 = require("./source/source24");
 var source25 = require("./source/source25");
 var source26 = require("./source/source26");
+var source27 = require("./source/source27");
 
 var arr = [];
 
@@ -55,6 +56,7 @@ arr.push(source23);
 arr.push(source24);
 arr.push(source25);
 arr.push(source26);
+arr.push(source27);
 
 
 var str = JSON.stringify(arr);
