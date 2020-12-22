9c553c67e9c56a7c151a8b33d201ca6bef9c91dc
tomsj <csj504@qq.com>
2020-12-22 10:39:24 +0800

    add

--- yiciyuan/source1.js
+++ yiciyuan/source1.js
@@ -7,41 +7,35 @@ var source36 = {
   'function getHeader(){return Headers.of("user-agent","Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")}function getHeader2(url){return getHeader()}function getHeader3(){return getHeader()};',
  
   // 漫画详情
-  ruleComicInfoUrl: 'http://m.kuman55.com/%s/?@Header:{"user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"}',
-  ruleComicTitle: '@css:h1.txtItme@text',
-  ruleComicAuthor: '@css:.sub_r .txtItme:nth-child(2)@text@js:result.replace("作者：","")',
+  ruleComicInfoUrl: 'http://yunmanhua2.yueriji.com/app/index.php?i=2&c=entry&id=%s&do=show&m=pinzhi_mh@Header:{"user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"}',
+  ruleComicTitle: 'class.title@text',
+  ruleComicAuthor: '',
   ruleComicCover:
-  '@css:#Cover>img@src',
-  ruleComicInstro: '@css:.d-nowrap-clamp@text',
+  'class.bg@tag.img@src',
+  ruleComicInstro: 'class.body@text',
   ruleComicStatus: false,
-  ruleComicUpdate: '@css:.txtItme:nth-child(4)@text@js:result.replace("更新时间：","")',
-  ruleChapterList: '@css:#chapterList_ul_2 li',
+  ruleComicUpdate: '',
+  ruleChapterList: '-@css:#html_box .item',
   ruleChapterName: '@css:a@text',
   ruleChapterUrl: '@css:a@href',
-  ruleChapterAjax: `importClass(Packages.java.util.ArrayList);importPackage(Packages.com.reader.comic.model);importClass(Packages.com.reader.comic.soup.Node);importClass(Packages.com.reader.comic.utils.StringUtils);importPackage(Packages.java.lang);importPackage(Packages.org.json);importClass(Packages.okhttp3.FormBody);importClass(Packages.okhttp3.Headers);importClass(Packages.okhttp3.Request);importClass(Packages.okhttp3.RequestBody);importClass(Packages.okhttp3.OkHttpClient);function parseChapter(){var list=new ArrayList();var body=new Node(content);var cid=body.hrefWithSplit("#chapterList_ul_2 li:first-child a",0);var client=new OkHttpClient();var request=new Request.Builder().url("http://m.kuman55.com/bookchapter/?id="+cid+"&id2=4").addHeader("User-Agent","Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1").build();var response=client.newCall(request).execute();if(response.isSuccessful()){var chapterList=new JSONArray(response.body().string());for(var i=0;i<chapterList.length();i++){var json=chapterList.getJSONObject(i);var title=json.getString("name");var path="/"+cid+"/"+json.getString("id")+".html";list.add(new Chapter(title,path))}}return list};`,
+  ruleChapterAjax: ``,
 
   // 图片详情
   ruleContentUrl:
-  'http://m.kuman55.comparam2?@Header:{"user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"}',
+  'http://yunmanhua2.yueriji.com/app/param2?@Header:{"user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"}',
   ruleComicContent:
   `
   importClass(Packages.java.util.ArrayList)
 importPackage(Packages.com.reader.comic.model)
-importClass(Packages.com.reader.comic.utils.DecryptionUtils)
 importClass(Packages.com.reader.comic.utils.StringUtils)
-importClass(Packages.android.util.Log)
+importClass(Packages.com.reader.comic.soup.Node)
 function parseImages() {
     var list = new ArrayList();
-    var base_url = "http://image.xmanhua.com/";
-    var data = StringUtils.match("km5_img_url=\\'(.+)\\'", content, 1);
-    var image_urls = DecryptionUtils.base64Decrypt(data);
-    eval("var arr="+image_urls+";");
-    for (var i = 0; i < arr.length; i ++) {
-        var url = arr[i];
-        url = url.split("\\|");
-        url = url[1].replace("\\\\","");
-        list.add(new ImageUrl(i+1, url,false));
-        i++;
+    var body = new Node(content);
+    var img_list = body.list("figure.item");
+    for(var i = 0; i < img_list.size();i++) {
+      var node = img_list.get(i);
+      list.add(new ImageUrl(i+1,node.attr("data-original")));
     }
     return list;
 }
