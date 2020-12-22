d9bd95cd686a551383216a5008554612938bdbaa
tomsj <csj504@qq.com>
2020-12-22 10:56:37 +0800

    add

---
+++ test.js
@@ -0,0 +1,7 @@
+var content = `
+<div class="class-list">		<a href="/manga-list/">全部</a>		<a href="/manga-list-31-0-10/" class="active">热血</a>		<a href="/manga-list-26-0-10/">恋爱</a>		<a href="/manga-list-1-0-10/">校园</a>		<a href="/manga-list-2-0-10/">冒险</a>		<a href="/manga-list-25-0-10/">科幻</a>		<a href="/manga-list-11-0-10/">生活</a>		<a href="/manga-list-17-0-10/">悬疑</a>		<a href="/manga-list-15-0-10/">魔法</a>		<a href="/manga-list-34-0-10/">运动</a>	</div>
+
+
+`;
+
+var str = 
