18f0698827d3accdbf8977af81601fc9d2d447fe
tomsj <csj504@qq.com>
2020-12-23 18:09:42 +0800

    add

--- test.js
+++ test.js
@@ -1,21 +1,63 @@
-`
-eval(function(p, a, c, k, e, d) {
-  e = function(c) {
-      return (c < a ? "": e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
-  };
-  if (!''.replace(/^/, String)) {
-      while (c--) d[e(c)] = k[c] || e(c);
-      k = [function(e) {
-          return d[e]
-      }];
-      e = function() {
-          return '\\w+'
-      };
-      c = 1;
-  };
-  while (c--) if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
-  return p;
-} ('q p=[\'4://5.6.7/1/2/0/n.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/o.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/t.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/u.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/r.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/s.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/m.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/g.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/h.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/f.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/e.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/k.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/l.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/j.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/i.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/F.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/G.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/D.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/E.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/J.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/K.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/H.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/I.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/x.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/y.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/v.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/w.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/B.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/C.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/z.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/A.3?8=0&c=d&9=1&a=b\'];', 47, 47, '119988||73|jpg|http|image|xmanhua|com|cid|type|uk|07899030407A56CCDB0A202449B7AC6C38A61C22286DA2F2FD47E543EABFD1D3FCC18D2B390E8D0B|key|dcf625ec0d403f390fa75de71bb01134|11_2808|10_9412|8_8572|9_3065|15_6889|14_9957|12_5151|13_6986|7_3336|1_5521|2_1720|newImgs|var|5_1349|6_2027|3_3170|4_6083|26_8189|27_3648|24_6458|25_5774|30_6382|31_3840|28_2741|29_2094|18_6613|19_7352|16_5410|17_3231|22_3292|23_8917|20_5493|21_2297'.split('|'), 0, {}))
+var content = `
+
+<!DOCTYPE html><html lang="en"><head>    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">    <meta name="format-detection" content="telephone=no">    <meta charset="UTF-8">    <title>鬼滅之刃漫畫_第205話_鬼滅之刃漫畫在線閱讀</title>    <meta name="Description" content="時值日本大正時期。
+傳說太陽下山后，惡鬼出沒吃人。..." />    <meta name="Keywords" content="鬼滅之刃,鬼滅之刃漫畫,鬼滅之刃漫畫免費閱讀" />    <link rel="stylesheet" type="text/css" href="http://css.xmanhua.com/v202011241431/xmanhua/css/mobile/chapterreset.css">    <link rel="stylesheet" type="text/css" href="http://css.xmanhua.com/v202011241431/xmanhua/css/mobile/chaptermstyle.css">    <script src="http://css.xmanhua.com/v202011241431/xmanhua/js/mobile/jquery-1.9.0.js"></script>    <script type="text/javascript" src="http://css.xmanhua.com/v202011241431/xmanhua/js/mobile/jquery.cookie.js"></script>    <script type="text/javascript" src="http://css.xmanhua.com/v202011241431/xmanhua/js/mobile/script.js"></script>    <script type="text/javascript">        var CSSHOST = "http://css.xmanhua.com/v202011241431/xmanhua/";        var mid = "73";        var cid = "119988";        var XMANHUA_MID=73;        var XMANHUA_CID=119988;        var USER_ID = 0;        var XMANHUA_USERID = 0;        var XMANHUA_CURRENTURL = "/m119988-p31/";        var XMANHUA_READMODE=1;        var XMANHUA_CHAPTERENDURL="/m119988-end/";        var isBar = true;    </script></head><body class="viewbody">    <div class="top-bar">        <div class="top-bar-tool">            <a href="javascript:history.back();"><img src="http://css.xmanhua.com/v202011241431/xmanhua/images/mobile/top-back-w.png" class="top-bar-tool-back"></a>            <p class="top-title">Mangabz?鬼滅之刃?第205話</p>        </div>    </div>    <div class="view-main-1 readForm" id="cp_img">    </div>    <div class="bottom-bar">        <div class="bottom-bar-tool"><a href="/m119638/" class="btn-2">上一章</a><a href="/m147828/" class="btn-2">下一章</a>        </div>    </div>    <div class="toast-win" style="display: none;">當前章節已讀完</div>    <script type="text/javascript">eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('q p=[\'4://5.6.7/1/2/0/n.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/o.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/t.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/u.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/r.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/s.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/m.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/g.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/h.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/f.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/e.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/k.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/l.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/j.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/i.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/F.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/G.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/D.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/E.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/J.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/K.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/H.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/I.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/x.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/y.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/v.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/w.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/B.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/C.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/z.3?8=0&c=d&9=1&a=b\',\'4://5.6.7/1/2/0/A.3?8=0&c=d&9=1&a=b\'];',47,47,'119988||73|jpg|http|image|xmanhua|com|cid|type|uk|07899030407A56CCDB0A202449B7AC6C38A61C22286DA2F23EB33EA97F86D22D67A37026DF7B0E1D|key|dcf625ec0d403f390fa75de71bb01134|11_2808|10_9412|8_8572|9_3065|15_6889|14_9957|12_5151|13_6986|7_3336|1_5521|2_1720|newImgs|var|5_1349|6_2027|3_3170|4_6083|26_8189|27_3648|24_6458|25_5774|30_6382|31_3840|28_2741|29_2094|18_6613|19_7352|16_5410|17_3231|22_3292|23_8917|20_5493|21_2297'.split('|'),0,{}))
+</script>    <script src="http://css.xmanhua.com/v202011241431/xmanhua/js/mobile/lazyloadimg.js"></script>    <script src="http://css.xmanhua.com/v202011241431/xmanhua/js/mobile/chapter.js"></script></body></html><div style="display:none">    <script type="text/javascript">
+        (function () {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src =('https:' == document.location.protocol ? 'https://' : 'http://') + "w.cnzz.com/c.php?id=1279350987";
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script><script type="text/javascript">
+        (function () {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src =('https:' == document.location.protocol ? 'https://' : 'http://') + "w.cnzz.com/c.php?id=1279350990";
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script><script type="text/javascript">
+        (function () {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src =('https:' == document.location.protocol ? 'https://' : 'http://') + "w.cnzz.com/c.php?id=1279350993";
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script></div>
 
 
-`
+
+`;
+
+
+
+
+var reg = /\}\('(.+\];)',([0-9]*),([0-9]*),('.+'\)),([0-9]*),(\{.*\})/g;
+var params = reg.exec(content);
+params[1] = params[1].replace(/\\/g, "");
+eval(get_img(params[1], params[2], params[3], params[4], 0, {}));
+
+console.log(newImgs);
+
+
+function get_img (p, a, c, k, e, d) {
+    eval("k="+k+";")
+    e = function(c) {
+        return (c < a ? '': e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+    };
+    if (!''.replace(/^/, String)) {
+        while (c--) {
+            d[e(c)] = k[c] || e(c)
+        }
+        k = [function(e) {
+            return d[e]
+        }];
+        e = function() {
+            return '\\w+'
+        };
+        c = 1
+    };
+    while (c--) {
+        if (k[c]) {
+            p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c])
+        }
+    }
+    return p
+  }
