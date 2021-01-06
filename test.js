42c64bef6aeddab70ec21b46f47790d287ff5cc7
tomsj <csj504@qq.com>
2021-01-06 08:36:10 +0800

    add

--- test.js
+++ test.js
@@ -1,176 +1,196 @@
-var LZString = function() {
-    function o(o, r) {
-        if (!t[o]) {
-            t[o] = {};
-            for (var n = 0; n < o.length; n++) t[o][o.charAt(n)] = n
+var template = `
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"><title>第283话上 秦陵_元尊漫画 - 奇漫屋</title>
+<meta name="keywords" content="第283话上 秦陵_元尊_免费看全部章节">
+<meta name="description" content="元尊漫画已更新到最新章节，免费看元尊漫画第283话上 秦陵就上奇漫屋，全集章节目录免费阅读,  下拉式页面清爽简洁，清新干净的文字章节在线阅读，让阅读成为一种享受。">
+<meta content="yes" name="apple-mobile-web-app-capable">
+<meta content="yes" name="apple-touch-fullscreen">
+<meta content="telephone=no,email=no" name="format-detection">
+<meta name="apple-mobile-web-app-title" content="奇漫屋">
+<meta name="screen-orientation" content="portrait">
+<meta name="x5-orientation" content="portrait">
+<meta http-equiv="Cache-Control" content="no-siteapp" />
+<meta http-equiv="Cache-Control" content="no-transform" />
+<meta itemprop="name" content="第283话上 秦陵_元尊漫画第283话上 秦陵_元尊漫画免费阅读_奇漫屋">
+<link rel="canonical" href="http://www.qiman5.com/12780/1216061.html">
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="/css/qiman5_all.css?v=1.736523">
+<script src="/js/warn_meta.js?v=3.3"></script>
+       <script src="/js/jquery.js"></script>
+        <script src="/js/swiper.js"></script>
+        <script src="/js/h5.base.js?v=1.536559"></script>
+</head>
+<body class="p-read" data-page_id="101">
+<script type="text/javascript">
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('8 a=["2://0.1.3/4/7","2://0.1.3/4/5","2://0.1.3/4/6","2://0.1.3/4/9","2://0.1.3/4/h","2://0.1.3/4/g","2://0.1.3/4/f","2://0.1.3/4/b","2://0.1.3/4/c","2://0.1.3/4/d","2://0.1.3/4/e"]',18,18,'p|pstatp|https|com|origin|137590001b4b28eb0a69e|ff0100030d68f7935bc3|fe5d000248527c5a67b7|var|138100001c9f557ef953a|newImgs|1381d00044181163a0ee2|137c5000207c54859e29e|1374800033e16c0c428a5|fee80002aedc5eca16d6|138e2000048c1ac0127d0|138ad000131a45b934df8|1377c00028939ab954c6c'.split('|'),0,{}))
+</script>
+<div class="top-tool-bar">
+    <a class="back-btn" href="/12780/"><i class="ift-back"></i></a>
+    <h1 class="comic-name">第283话上 秦陵</h1>
+    <a class="home" href="/"><i class="ift-read_home"></i></a>
+</div>
+<div class="guide-download">
+ <img src="https://p.pstatp.com/origin/1375200000cd4a1996591" alt="" class="logo"> 
+ <div class="tips"><p class="van-p_1">安装奇漫屋APP</p>
+ <p class="van-p_2">奇漫屋APP正式上线了,你想看的漫画都在这</p></div> <a href="http://apk.qqvv88.com/down/qimanwu.apk" class="">
+ <button hollow="" class="download-btn"><span class="van-button__text">APP下载</span></button></a></div><main id="mainView" class="read-container" data-number="5" data-comic_name="元尊" data-comic_type="1">
+    <ul class="comic-list">
+            </ul>
+    <p class="read-pos">第283话上 秦陵 <span class="comic-a">11P</span></p>
+</main>
+<!-- 章节结束 -->
+<div class="chapter-end">
+    <p class="end-txt"><span>您已阅读完：</span>第283话上 秦陵</p>
+    <div class="end-btns">
+        <ul class="clearfix">
+            <li class="end-itm prev"><a href="/12780/1215873.html"><i class="i-rd-prev"></i>上一章</a></li>
+            <li class="end-itm"><a href="/12780/"><i class="i-rd-mid"></i>返回详情</a></li>
+            <li class="end-itm next"><a href="/12780/"><i class="i-rd-next"></i>下一章</a></li>
+        </ul>
+    </div>
+</div>
+<script src="http://rhmovie.4kyu.cn/qimanlook.js"></script><!-- 完结作品/连载未完结 渲染不同 -->
+<div class="read-end" style="height: auto;">
+    <!-- 分割线 -->
+    <div class="gap"></div>
+    <!-- 相关推荐 -->
+    <div class="comic-recommend">
+        <p class="title">相关推荐</p>
+        <ul class="clearfix">
+            <li class="rec-item">
+                                <div class="cover">
+                                    <a href="/16041/"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="https://p.pstatp.com/origin/ffbc0002b033d6f76f81" alt="武炼巅峰"></a>
+                                </div>
+                                <p class="comic-name">武炼巅峰</p>
+                            </li><li class="rec-item">
+                                <div class="cover">
+                                    <a href="/9845/"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="https://p.pstatp.com/origin/ff8d000147a545b72a1e" alt="全职法师"></a>
+                                </div>
+                                <p class="comic-name">全职法师</p>
+                            </li><li class="rec-item">
+                                <div class="cover">
+                                    <a href="/10307/"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="https://p.pstatp.com/origin/fef20000e4d549c86bb7" alt="百炼成神"></a>
+                                </div>
+                                <p class="comic-name">百炼成神</p>
+                            </li><li class="rec-item">
+                                <div class="cover">
+                                    <a href="/12670/"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="https://p.pstatp.com/origin/fe360001a3e066e50ab5" alt="重生之都市修仙"></a>
+                                </div>
+                                <p class="comic-name">重生之都市修仙</p>
+                            </li><li class="rec-item">
+                                <div class="cover">
+                                    <a href="/12693/"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="https://p.pstatp.com/origin/ff910000e6fa49f719f3" alt="斗罗大陆"></a>
+                                </div>
+                                <p class="comic-name">斗罗大陆</p>
+                            </li><li class="rec-item">
+                                <div class="cover">
+                                    <a href="/12624/"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="https://p.pstatp.com/origin/fe93000133643e85213b" alt="我家大师兄脑子有坑"></a>
+                                </div>
+                                <p class="comic-name">我家大师兄脑子有坑</p>
+                            </li>        </ul>
+    </div>
+</div>
+<style>
+
+</style>
+<div class="gap2"></div>
+<!--评论区-->
+<div class="comment-box hide" style="display: block;padding: .61333rem 0 0.15333rem;">
+    <p class="jcpl">精彩评论</p>
+    <div class="no-comment">
+        <div class="no-comment-bg"></div>
+        <p class="txt">主人还等什么，快畅聊一番吧～</p>
+    </div>
+    <div class="comment-wrap" id="last-read-pinglun">
+    </div> 
+</div>
+<div class="comment-input-box" style="padding-bottom: .48rem; position: inherit;">
+    <p class="pinglun-btn">呦吼，评论来一发</p>
+</div>
+<!--评论区-->
+<script type="text/javascript">var COMIC_MID_NUM = 0;</script>
+<!--  评论弹出输出  -->
+<div class="comment-layout hide">
+    <div class="header">
+        <i class="ift-nav_close"></i>
+        <span>发表评论</span>
+    </div>
+    <div class="area-wrap">
+        <textarea name="comment" id="commentArea"></textarea>
+    </div>
+    <div class="comment-sumit">发布</div>
+    <div class="face-wrapper hide">
+        <div class="face-list"></div>
+        <div class="face-group clearfix"></div>
+    </div>
+</div>
+<div class="bottom-tool-bar">
+    <ul class="clearfix">
+        <li class="collect" id="collect"><a href="/user/add/?novelid=12780&ctid=1216061&ctname=第283话上 秦陵">
+            <i class="ift-detail_coll_off"></i>
+            <span class="txt">收藏</span></a> 
+        </li>
+        <li class="switch"> <a href="/user/history/">
+            <i class="ift-read_tab_flip"></i>
+            <span>书架</span></a>
+        </li>
+        <li class="share"> <a href="http://apk.qqvv88.com/down/qimanwu.apk">
+            <i class="ift-detail_share"></i>
+            <span>下载APP</span></a>
+        </li>
+        <li class="prev-chapter">
+            <a href="/12780/1215873.html">  <i class="ift-read_magic_l"></i>
+                <span>上话</span></a>
+        </li>
+        <li class="next-chapter">
+            <a href="/12780/">
+                <i class="ift-read_magic_r"></i>
+                <span>下话</span></a>
+        </li>
+    </ul>
+</div>
+<script src="/js/qiman5.core.js?v=1.5963266"></script>
+<script>
+     firstLoad();           
+    var pinglun_id="12780";
+/*      $(document).ready(function (){$(document).ready(function (){$.get("/pinglun/?id=12780", function(data){$("#last-read-pinglun").prepend(data);});});});*/
+</script>
+<script src="/js/jquery.lazyload.min.js?v=3.0"></script>
+<div style="display:none"><script src="/js/tj.js" language="JavaScript"></script></div></body>
+</html>
+
+`;
+
+var reg = /\}\('(.+\])',([0-9]*),([0-9]*),('.+'\)),([0-9]*),(\{.*\})/g;
+var params = reg.exec(template);
+params[1] = params[1].replace(/\\/g, "");
+
+console.log(get_img(params[1], params[2], params[3], params[4], 0, {}));
+
+
+function get_img(p, a, c, k, e, d) {
+    e = function(c) {
+        return (c < a ? '': e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+    };
+    if (!''.replace(/^/, String)) {
+        while (c--) {
+            d[e(c)] = k[c] || e(c)
         }
-        return t[o][r]
+        k = [function(e) {
+            return d[e]
+        }];
+        e = function() {
+            return '\\w+'
+        };
+        c = 1
+    };
+    while (c--) {
+        if (k[c]) {
+            p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c])
+        }
     }
-    var r = String.fromCharCode,
-    n = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-    e = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-$",
-    t = {},
-    i = {
-        decompressFromBase64: function(r) {
-            return null == r ? "": "" == r ? null: i._decompress(r.length, 32,
-            function(e) {
-                return o(n, r.charAt(e))
-            })
-        },
-        decompressFromUTF16: function(o) {
-            return null == o ? "": "" == o ? null: i._decompress(o.length, 16384,
-            function(r) {
-                return o.charCodeAt(r) - 32
-            })
-        },
-        decompressFromUint8Array: function(o) {
-            if (null === o || void 0 === o) return i.decompress(o);
-            for (var n = new Array(o.length / 2), e = 0, t = n.length; t > e; e++) n[e] = 256 * o[2 * e] + o[2 * e + 1];
-            var s = [];
-            return n.forEach(function(o) {
-                s.push(r(o))
-            }),
-            i.decompress(s.join(""))
-        },
-        compressToEncodedURIComponent: function(o) {
-            return null == o ? "": i._compress(o, 6,
-            function(o) {
-                return e.charAt(o)
-            })
-        },
-        decompressFromEncodedURIComponent: function(r) {
-            return null == r ? "": "" == r ? null: (r = r.replace(/ /g, "+"), i._decompress(r.length, 32,
-            function(n) {
-                return o(e, r.charAt(n))
-            }))
-        },
-  
-        decompress: function(o) {
-            return null == o ? "": "" == o ? null: i._decompress(o.length, 32768,
-            function(r) {
-                return o.charCodeAt(r)
-            })
-        },
-        _decompress: function(o, n, e) {
-            var t, i, s, p, u, c, a, l, f = [],
-            h = 4,
-            d = 4,
-            m = 3,
-            v = "",
-            w = [],
-            A = {
-                val: e(0),
-                position: n,
-                index: 1
-            };
-            for (i = 0; 3 > i; i += 1) f[i] = i;
-            for (p = 0, c = Math.pow(2, 2), a = 1; a != c;) u = A.val & A.position,
-            A.position >>= 1,
-            0 == A.position && (A.position = n, A.val = e(A.index++)),
-            p |= (u > 0 ? 1 : 0) * a,
-            a <<= 1;
-            switch (t = p) {
-            case 0:
-                for (p = 0, c = Math.pow(2, 8), a = 1; a != c;) u = A.val & A.position,
-                A.position >>= 1,
-                0 == A.position && (A.position = n, A.val = e(A.index++)),
-                p |= (u > 0 ? 1 : 0) * a,
-                a <<= 1;
-                l = r(p);
-                break;
-            case 1:
-                for (p = 0, c = Math.pow(2, 16), a = 1; a != c;) u = A.val & A.position,
-                A.position >>= 1,
-                0 == A.position && (A.position = n, A.val = e(A.index++)),
-                p |= (u > 0 ? 1 : 0) * a,
-                a <<= 1;
-                l = r(p);
-                break;
-            case 2:
-                return ""
-            }
-            for (f[3] = l, s = l, w.push(l);;) {
-                if (A.index > o) return "";
-                for (p = 0, c = Math.pow(2, m), a = 1; a != c;) u = A.val & A.position,
-                A.position >>= 1,
-                0 == A.position && (A.position = n, A.val = e(A.index++)),
-                p |= (u > 0 ? 1 : 0) * a,
-                a <<= 1;
-                switch (l = p) {
-                case 0:
-                    for (p = 0, c = Math.pow(2, 8), a = 1; a != c;) u = A.val & A.position,
-                    A.position >>= 1,
-                    0 == A.position && (A.position = n, A.val = e(A.index++)),
-                    p |= (u > 0 ? 1 : 0) * a,
-                    a <<= 1;
-                    f[d++] = r(p),
-                    l = d - 1,
-                    h--;
-                    break;
-                case 1:
-                    for (p = 0, c = Math.pow(2, 16), a = 1; a != c;) u = A.val & A.position,
-                    A.position >>= 1,
-                    0 == A.position && (A.position = n, A.val = e(A.index++)),
-                    p |= (u > 0 ? 1 : 0) * a,
-                    a <<= 1;
-                    f[d++] = r(p),
-                    l = d - 1,
-                    h--;
-                    break;
-                case 2:
-                    return w.join("")
-                }
-                if (0 == h && (h = Math.pow(2, m), m++), f[l]) v = f[l];
-                else {
-                    if (l !== d) return null;
-                    v = s + s.charAt(0)
-                }
-                w.push(v),
-                f[d++] = s + v.charAt(0),
-                h--,
-                s = v,
-                0 == h && (h = Math.pow(2, m), m++)
-            }
-        }
-    };
-    return i
-} ();
-"function" == typeof define && define.amd ? define(function() {
-    return LZString
-}) : "undefined" != typeof module && null != module && (module.exports = LZString);
-
-
-
-let img_data = "JYWw5g9AzAHA7FATBArFAbI9AWCAGARhQDoArABzABpRJYFk1Md8C4zKbxp4lUMsuQgE4O1Wjwb9mQxHjFc6vRgJZ5EBBRPp8mg/IkRbuOlTINRjSqXrWJsVybtWySFcSeXT969I9Peduzuik5mPogw/l62sqIh2jEu+FDyCZ42yXhQmunWzubZRnlhgUJQliUBsSkOVUmFUG6ciZmNfvVtPlDBLRkF3VGdA2pQ8X354WrYaROlNXjYuXPVWdjFKw0+2JWbXdN1eyNC2M0ek2X42B1HUye95/NrQ7eXi+OPq4Uos59baihln99kIUBtgcd8GhoiCoYcIXcoWdQl8fCgbgi3igHij/qCXpiFigPrjYXh0L9SZDyUCqYjyeC6W90LtCVl0MjWtT0BimQt0DiufT0AS+eySUK3nBfkA"
-
-var params_data = ['json', '&_KE=', '.pjax-container', 'chapter_num', 'parent', 'lazyload', 'decompressFromBase64', 'html', '\x22\x20href=\x22javascript:;\x22\x20onclick=\x22vg_pjax($(this));\x22\x20class=\x22btn\x20btn-primary\x20mb-1\x20pnext\x20next\x22\x20title=\x22下一页\x22>下页</a>\x0a\x09\x09<a\x20href=\x22javascript:goNumPage(\x27next\x27);\x22\x20class=\x22btn\x20btn-primary\x20mb-1\x20ml-1\x22\x20title=\x22下集\x22>下集</a>\x0a\x09\x09</div>\x0a\x09\x09</nav>\x0a\x09', 'asc', '\x22\x20title=\x22上一页\x22>上页</a>\x0a\x09\x09<div\x20class=\x22mx-1\x20mb-1\x22>\x0a\x09\x09<label\x20for=\x22page-selector\x22\x20class=\x22sr-only\x22>翻页</label>\x0a\x09\x09<select\x20class=\x22form-control\x20vg-page-selector\x22\x20id=\x22page-selector\x22\x20onchange=\x22vg_pjax($(this),\x201);\x22>\x0a\x09', 'ajax', '\x0a\x09\x09</select>\x0a\x09\x09</div>\x0a\x09\x09<a\x20data-p=\x22', '/uploads/', 'val', '/chapter_num?chapter_id=', '.pre', 'pre', 'location', '页</option>', 'round', 'next', 'preload', '\x0a\x09\x09<nav\x20aria-label=\x22Page\x20navigation\x22>\x0a\x09\x09<div\x20class=\x22form-inline\x22>\x0a\x09\x09<a\x20href=\x22javascript:goNumPage(\x27pre\x27);\x22\x20class=\x22btn\x20btn-primary\x20mb-1\x20mr-1\x22\x20title=\x22上集\x22>上集</a>\x0a\x09\x09<a\x20data-p=\x22', '.sort', 'chapter-type', 'url', 'split', '&type=', 'data', 'click', 'length', 'chapter-domain', 'get', '.vg-r-data', 'fadeIn', 'keydown', 'chapter-key', 'which', '没有啦，去发表下评论吧', '.show-pic', 'attr', 'href', 'push', 'replace', 'selected', '?_KS=', '.html', '<option\x20value=\x22', 'src', 'code'];
-(function(a, b) {
-    var d = function(t) {
-        while (--t) {
-            a['push'](a['shift']());
-        }
-    };
-    d(++b);
-}(params_data, 0x112));
-
-
-var by = function(a, b) {
-    a = a - 0x0;
-    var c = params_data[a];
-    return c;
-};
-//[Function: decompressFromBase64]
-console.log(LZString[by('0x26')]);
-console.log(LZString.decompressFromBase64(img_data));
-
-var base = "https://p1.manhuacat.com/uploads/";
-
-let img_data_arr = LZString[by('0x26')](img_data)[by('0x8')](',');
-
-// for (var i = 0; i < img_data_arr.length; i ++) {
-//     console.log("https://p1.manhuacat.com/uploads/"+img_data_arr[i]);
-// }
-
-
-
-
+    return p
+}
