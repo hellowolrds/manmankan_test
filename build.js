d065e2fca74c266366f13be32f47223dc05438a2
tomsj <csj504@qq.com>
2021-01-25 09:36:27 +0800

    add

--- build.js
+++ build.js
@@ -106,7 +106,6 @@ var source96 = require("./source2/source5");
 var source97 = require("./source2/source6");
 var source98 = require("./source2/source7");
 var source99 = require("./source2/source8");
-var source100 = require("./source2/source9");
 
 var arr = [];
 
@@ -213,7 +212,6 @@ arr.push(source96);
 arr.push(source97);
 arr.push(source98);
 arr.push(source99);
-arr.push(source100);
 
 
 var net_arr = [];
